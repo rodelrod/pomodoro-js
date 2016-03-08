5669d7db7ba4dddd8b44622399516d1379de2796
rodelrod <rodelrod@hotmail.com>
2016-03-08 15:19:01 +0100

    Package for Linux

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const BrowserWindow = electron.BrowserWindow;
 var mainWindow = null;
 
 app.on('ready', function() {
-  mainWindow = new BrowserWindow({
-    width: 300,
-    height: 200
-  })
-
-  mainWindow.loadURL('file://' + __dirname + '/public/index.html');
+    mainWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        icon: __dirname + '/public/assets/icons/clock-128x128.png'
+    })                                             
+                                                   
+mainWindow.loadURL('file://' + __dirname + '/public/index.html');
 
   //mainWindow.webContents.openDevTools();
 
