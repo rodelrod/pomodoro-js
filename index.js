a4320702612805a7a3e59a306ae3c8b63642bf00
rodelrod <rodelrod@hotmail.com>
2016-03-25 14:52:04 +0100

    Make electron window close cleanly

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
 var mainWindow = null;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
 
 app.on('ready', function() {
     mainWindow = new BrowserWindow({
@@ -21,5 +32,13 @@ app.on('ready', function() {
     mainWindow.loadURL('file://' + __dirname + '/public/index.html');
 
     //mainWindow.webContents.openDevTools();
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
 
 });
